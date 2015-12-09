7eb4af87075b190b8befa53915c30e978b545d69
akhmetzyan <akhmetzyan@gmail.com>
2015-12-09 10:16:04 +0300

    Yep

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
 			return false;
 		}
 		formData = new FormData($('#uploader')[0]);
+		$('#startWork').html('Обработка...')
 		$.ajax({
 			url: '/uploadfiles',
 			type: "POST",
 			data: formData,
 			processData: false,
 			contentType: false,
-			success: function(data, textStatus, jqXHR){if (data.status){window.open('/getcsv', '_blank'); window.open('/getzip', '_blank')}},
+			success: function(data, textStatus, jqXHR){if (data.status){window.open('/getcsv', '_blank'); window.open('/getzip', '_blank');$('#startWork').html('Обработать')}},
 			error: function(jqXHR, textStatus, errorThrown){alert('Произошла ошибка сервера');}
 		});
 		return false;
